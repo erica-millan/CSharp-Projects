1bfd54268803910d02da1f252b15266bc5fbcc9e
Erica Millan <ericamillan1234@gmail.com>
2026-02-06 14:06:52 -0600

    Anonymous Income Comparison Program

---
+++ Anonymous-Income-Comparison-Program/Anonymous-Income-Comparison-Program/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Income_Comparison_Program
+{
+    //Person class with properties and method to calculate annual salary
+    public class Person
+    {
+        //properties to hold the hourly rate and weekly hours worked for each person
+        //getters and setters allow us to access and modify these
+        //properties when creating person objects and calculating their annual salaries
+        public decimal HourlyRate { get; set; }
+        public decimal WeeklyHoursWorked { get; set; }
+
+
+        //constructor runs when you create an instance of the class (person object)
+        //and it assigns values to the objects properties with the
+        //values passed in as parameters when creating the object
+
+        //Constructor for the Person class that takes hourly rate and weekly hours worked as parameters
+        public Person(decimal hourlyRate, decimal weeklyHoursWorked)
+        {
+            HourlyRate = hourlyRate;
+            WeeklyHoursWorked = weeklyHoursWorked;
+        }
+
+        //Method that returns the annual salary based on the hourly rate and weekly hours worked
+        //main program will call this method with the hourly rate and weekly hours worked for each
+        //person as parameters and it will return the calculated annual salary for each person
+        public static decimal CalculateAnnualSalary(decimal hourlyRate, decimal weeklyHours)
+        {
+            return hourlyRate * weeklyHours * 52;
+        }
+
+        //this method gets called with the annual salaries of p 1 and p 2, compared and returns true or false to main program
+        public static bool p1GreaterThanp2(decimal p1AnnualSalary, decimal p2AnnualSalary)
+        {
+            if (p1AnnualSalary > p2AnnualSalary)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Anonymous-Income-Comparison-Program/Anonymous-Income-Comparison-Program/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Income_Comparison_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+            //gettuing user input for person 1 and person 2
+            Console.WriteLine("Person 1 hourly rate?");
+            decimal p1HourlyRate = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Person 1 hours worked per week?");
+            decimal p1WeeklyHoursWorked = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Person 2 hourly rate?");
+            decimal p2HourlyRate = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Person 2 hours worked per week?");
+            decimal p2WeeklyHoursWorked = Convert.ToDecimal(Console.ReadLine());
+
+            //Creating a person using the Person class constructor for neater code and to
+            //practice the using classes and constucter for c#. (learned in python course)
+            //creating person 1 and person 2 objects using the Person class constructor
+            //they will hold the information/properties of the objects and
+            //we can use them to call the method to calculate the annual salary
+            Person person1 = new Person(p1HourlyRate, p1WeeklyHoursWorked);
+            Person person2 = new Person(p2HourlyRate, p2WeeklyHoursWorked);
+
+            // Call the calculate annual salary method for both persons
+            // and store the results in corresponding variables
+            decimal p1AnnualSalary = Person.CalculateAnnualSalary(p1HourlyRate, p1WeeklyHoursWorked);
+            decimal p2AnnualSalary = Person.CalculateAnnualSalary(p2HourlyRate, p2WeeklyHoursWorked);
+
+            // Display results
+            Console.WriteLine("Person 1 annual salary: " + p1AnnualSalary);
+            Console.WriteLine("Person 2 annual salary: " + p2AnnualSalary);
+
+            //creating a boolean to store the result of calliing the method p1GreaterThanp2 to compare the annual salaries of person 1 and person 2
+            //Method returns the result (true or false) and it gets stored in bool
+            bool person1MakesMoreMoneyThanPerson2 = Person.p1GreaterThanp2(p1AnnualSalary, p2AnnualSalary);
+
+            //outputting that result that was stored in boolean
+            Console.WriteLine("Does Person 1 make more money than Person 2? " + person1MakesMoreMoneyThanPerson2);
+
+        }
+    }
+}
