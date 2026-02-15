d99673b271b428acf94eecdf66b88553c2500cac
Erica Millan <ericamillan1234@gmail.com>
2026-02-15 15:45:15 -0600

    Submitting calling methods assignment

---
+++ calling_methods_assignment/Calling_methods_assignment/Math_Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_methods_assignment
+{
+    public class Math_Operations
+    {   //creating all methods
+        //All methods take in paramter named num which will be user input
+        public static decimal AddTen(decimal num)
+        {   //add 10 to user input and return
+            return num + 10 ;
+        }
+
+        public static decimal MultiplyByTen(decimal num)
+        {   //mult by 10 user input and return
+            return num * 10;
+        }
+
+        public static decimal DivideByTwo(decimal num)
+        {   //divide by 2 user input and return
+            return num / 2 ;
+        }
+
+    }
+}
---
+++ calling_methods_assignment/Calling_methods_assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Calling_methods_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //get operand from user
+            Console.WriteLine("What number do you want to do math on?");
+            string userInput = Console.ReadLine();
+
+            //convert the string to decimal
+            decimal operand = Convert.ToDecimal(userInput);
+
+            //calling each method storing returned value in corresponding variables
+            decimal sum = Math_Operations.AddTen(operand);
+            decimal product = Math_Operations.MultiplyByTen(operand);
+            decimal quotient = Math_Operations.DivideByTwo(operand);
+
+            // displaying the returned integer to the console
+
+            Console.WriteLine("Users selected number plus 10 is: " + sum );
+            Console.WriteLine("Users selected number times 10 is: " + product );
+            Console.WriteLine("Users selected number divided by 2 is: " + quotient );
+        }
+    }
+}
