4a200cb82cefca53fb31130f6583f9e271568822
Erica Millan <ericamillan1234@gmail.com>
2026-02-17 14:05:15 -0600

    making changes to my program to fix it

--- main_method_assignment/Main_Method_Assignment/Math_Operations.cs
+++ main_method_assignment/Main_Method_Assignment/Math_Operations.cs
@@ -11,20 +11,20 @@ namespace Main_Method_Assignment
         //all methods have the same name
 
         //takes an integer does math and returns an int
-        public static int mathOp(int num) 
+        public int mathOp(int num) 
         {
             return num + 10;
         }
 
         //takes in a decimal, does math and converts to int and returns as an int
-        public static int mathOp(decimal num)
+        public int mathOp(decimal num)
         {
             decimal result = num / 2;
             return (int) result;
         }
 
         //takes in a string converts to int does math and returns as an intr
-        public static int mathOp(string num)
+        public int mathOp(string num)
         {
             int converted = Convert.ToInt32(num);
             return converted * 10;
--- main_method_assignment/Main_Method_Assignment/Program.cs
+++ main_method_assignment/Main_Method_Assignment/Program.cs
@@ -9,27 +9,44 @@ namespace Main_Method_Assignment
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //callingi nt method
+        {   
+            //instantiating the class
+            Math_Operations ops = new Math_Operations();
+
+            //calling int method
             int intOperand = 10;
-            //instantaiting the class, calling methoding passing in an int
-            int intResult = Math_Operations.mathOp(intOperand);
+            //calling methoding passing in an int
+            int intResult = ops.mathOp(intOperand);
             //display result to the console
             Console.WriteLine("Result of the int method: " + intResult);
 
             //calling decimal method
             decimal decOperand = 20.5m;
-            //instantiating the class, calling methoding passing in a dec
-            int decResult = Math_Operations.mathOp(decOperand);
+            //calling methoding passing in a dec
+            int decResult = ops.mathOp(decOperand);
             //display result to the console
             Console.WriteLine("Result of the decimal method: " + decResult);
 
             string strOperand = "10";
-            //instantaiting the class, calling methoding passing in a string
-            int stringResult = Math_Operations.mathOp(strOperand);
+            //calling methoding passing in a string
+            int stringResult = ops.mathOp(strOperand);
             //display result to the console
             Console.WriteLine("Result of the string method: " + stringResult);
 
+           /*
+            * keeping incorrect code to compare later
+            int intOperand = 10;
+            int intResult = Math_Operations.mathOp(intOperand);
+            Console.WriteLine("Result of the int method: " + intResult);
+
+            decimal decOperand = 20.5m;
+            int decResult = Math_Operations.mathOp(decOperand);
+            Console.WriteLine("Result of the decimal method: " + decResult);
+
+            string strOperand = "10";
+            int stringResult = Math_Operations.mathOp(strOperand);
+            Console.WriteLine("Result of the string method: " + stringResult);
+           */
         }
     }
 }
