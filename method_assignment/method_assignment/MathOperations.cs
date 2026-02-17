26b5a8b277369391c271c0f02b6cec327a62b4bc
Erica Millan <ericamillan1234@gmail.com>
2026-02-17 16:32:50 -0600

    adding assignment with optional operand

---
+++ method_assignment/method_assignment/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_assignment
+{
+    public class MathOperations
+    {
+        //method addnumbers that takes 2 parameters, the second it optional it sets it = 1 if theres nothing
+        public int addNumbers(int op1, int op2) 
+        {
+            int result = op1 * op2;
+            return result;
+        }
+    }
+
+}
---
+++ method_assignment/method_assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating MathOperations class
+            MathOperations op = new MathOperations();
+
+            Console.WriteLine("Input your first number:");
+            int firstInput = Convert.ToInt32(Console.ReadLine());
+
+            //if user presses enter instead of a number it will return an empty string 
+            //have to read input as a string and check it ifs empty, only converting if it's not empty
+            Console.WriteLine("Input your second number: (this part is optional, press enter to bypass)");
+            string secondInputCheck = Console.ReadLine();
+
+            //checking if user entered a value 
+            int secondInput;
+            if (string.IsNullOrWhiteSpace(secondInputCheck))
+            {
+                //user didn't add input setting secondInput = 1;
+                secondInput = 1;
+            } 
+            else
+            {
+                // user added input need to convert it to int
+                secondInput = Convert.ToInt32(secondInputCheck);
+            }
+
+            //setting result = to the op object.add numbers which is the method addd numbers in the math operations class with the 2 user inputs in
+            int result = op.addNumbers(firstInput, secondInput);
+            //writing result to console
+            Console.WriteLine("Your first value times second value is: " + result);
+        }
+    }
+}
