e25961522edaf2b98520c7d0b82c3d6924e2b6c6
Erica Millan <ericamillan1234@gmail.com>
2026-02-17 20:07:26 -0600

    updating void project

--- VoidMethodAssignment/VoidMethodAssignment/Program.cs
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,14 @@ namespace VoidMethodAssignment
     {
         static void Main(string[] args)
         {
+            //instantiating mathoperations class setting instance equal to the "op" variable
+            MathOperations op = new MathOperations();
+
+            //calling method passing numbers
+            op.addNumbers(1, 1);
+
+            //calling methods using named paramters
+            op.addNumbers(op1: 7, input: 3);
         }
     }
 }
