5775e590a1702507d68276eaf1ebdba9da76afca
Erica Millan <ericamillan1234@gmail.com>
2026-03-16 13:00:04 -0500

    adding constructor chaining

---
+++ ConstructorChaining/ConstructorChaining/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//have to create another class in order to demonstrate constructor chaining.
+class Class1
+{
+    private int num1; //variable that stores a number
+                      // constructor that takes in 2 variables
+    public Class1(int num, string msg)
+    {
+        num1 = num;             
+        Console.WriteLine(msg);
+    }
+
+    //second constructor that takes 1 parameter
+    public Class1(int num)
+        //: this(num, message) meants before running this constructor run other constructor first (constructor chaining)
+        : this(num, "Outputting that 1 parameter called thr 2 parameter constructor.")
+    {
+       //other constructor does work that would have been here
+    }
+    //method that prints the number
+    public void ShowVariable()
+    {
+        Console.WriteLine("Stored variable : " + num1);
+    }
+}
---
+++ ConstructorChaining/ConstructorChaining/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        //constant variable, once this variable is set it can not be changed
+        const int constVar = 10;
+
+        //variable made with var keyword.
+        //This is c#'s way of implicitly typing, it looks for the type, in this case it's a number and it gives it the data type (int)
+        var varNum = 5;
+ 
+        //create an object of Class1 passihng in only one number 
+        //the constructor with 2 paramters will run automatically
+        Class1 obj = new Class1(constVar);
+
+        // call method on the object, this will print the stored variable
+        obj.ShowVariable();
+
+        Console.ReadLine();
+    }
+}
+
